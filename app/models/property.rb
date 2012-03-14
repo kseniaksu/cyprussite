bbf70dceaea309e2621981908daefc0d99a2f43e
Ksenia Ksu <kseniaksubilous@gmail.com>
2012-03-14 23:29:08 +0200

    new

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,7 +7,6 @@ class Property < ActiveRecord::Base
                     :bucket => 'photos.domnakipre.com.ua'
                     
 
-  validates_attachment_presence :photo
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
   
   has_attached_file :photo1, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -17,7 +16,6 @@ class Property < ActiveRecord::Base
                     :path => ":id/:style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo1
   validates_attachment_content_type :photo1, :content_type => ['image/jpeg', 'image/png']
 
   has_attached_file :photo2, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -27,7 +25,6 @@ class Property < ActiveRecord::Base
                     :path => ":id/:style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo2
   validates_attachment_content_type :photo2, :content_type => ['image/jpeg', 'image/png']
 
   has_attached_file :photo3, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -37,7 +34,6 @@ class Property < ActiveRecord::Base
                     :path => ":id/:style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo3
   validates_attachment_content_type :photo3, :content_type => ['image/jpeg', 'image/png']
 
   has_attached_file :photo4, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -47,7 +43,6 @@ class Property < ActiveRecord::Base
                     :path => ":id/:style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo4
   validates_attachment_content_type :photo4, :content_type => ['image/jpeg', 'image/png']
 
   has_attached_file :photo5, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -57,7 +52,6 @@ class Property < ActiveRecord::Base
                     :path => ":id/:style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo5
   validates_attachment_content_type :photo5, :content_type => ['image/jpeg', 'image/png']
 
   has_attached_file :photo6, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -67,7 +61,6 @@ class Property < ActiveRecord::Base
                     :path => ":id/:style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo6
   validates_attachment_content_type :photo6, :content_type => ['image/jpeg', 'image/png']
 
   has_attached_file :photo7, :styles => { :small => "93x53>", :medium => "154x88>" },
@@ -77,7 +70,6 @@ class Property < ActiveRecord::Base
                     :path => ":style/:basename.:extension",
                     :bucket => 'photos.domnakipre.com.ua'
 
-  validates_attachment_presence :photo7
   validates_attachment_content_type :photo7, :content_type => ['image/jpeg', 'image/png']
 
   

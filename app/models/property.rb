ae3ab733c1c11572db19cc67861adb40b4672d35
Ksenia Ksu <kseniaksubilous@gmail.com>
2012-03-15 23:54:27 +0200

    dfk

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :small => "93x53>", :medium => "154x88>" },                    
+  has_attached_file :photo, :styles => { :small => "93>x53", :medium => "154>x88", :onmaim => "174>x94" },                    
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -9,7 +9,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
   
-  has_attached_file :photo1, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo1, :styles => { :small => "93>x53", :medium => "154>x88", :onmaim => "174>x94" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -18,7 +18,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo1, :content_type => ['image/jpeg', 'image/png']
 
-  has_attached_file :photo2, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo2, :styles => { :small => "93>x53", :medium => "154>x88", :onmaim => "174>x94" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -27,7 +27,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo2, :content_type => ['image/jpeg', 'image/png']
 
-  has_attached_file :photo3, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo3, :styles => { :small => "93>x53", :medium => "154>x88", :onmaim => "174>x94" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -36,7 +36,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo3, :content_type => ['image/jpeg', 'image/png']
 
-  has_attached_file :photo4, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo4, :styles => { :small => "93>x53", :medium => "154>x88", :onmaim => "174>x94" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -45,7 +45,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo4, :content_type => ['image/jpeg', 'image/png']
 
-  has_attached_file :photo5, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo5, :styles => { :small => "93>x53", :medium => "154>x88", :onmaim => "174>x94"},
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -54,7 +54,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo5, :content_type => ['image/jpeg', 'image/png']
 
-  has_attached_file :photo6, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo6, :styles => { :small => "93>x53", :medium => "154>x88",  :onmaim => "174>x94" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:id/:style/:basename.:extension",
@@ -63,7 +63,7 @@ class Property < ActiveRecord::Base
 
   validates_attachment_content_type :photo6, :content_type => ['image/jpeg', 'image/png']
 
-  has_attached_file :photo7, :styles => { :small => "93x53>", :medium => "154x88>" },
+  has_attached_file :photo7, :styles => { :small => "93>x53", :medium => "154>x88",  :onmaim => "174>x94" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :url  => "/:style/:basename.:extension",
